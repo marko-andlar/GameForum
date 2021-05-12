c75ed7424af8bd6fd8951ae71e4869bb1f960328
Marko <marko.andlar@gmai.com>
2021-05-12 17:31:02 +0200

    Add ViewModel Library

---
+++ GameForum.ViewModel/CategoryPosts.cs
@@ -0,0 +1,11 @@
+﻿using GameForum.Model;
+using System.Collections.Generic;
+
+namespace GameForum.ViewModel
+{
+    public class CategoryPosts
+    {
+        public PostCategory Category { get; set; }
+        public List<Post> Posts { get; set; }
+    }
+}
