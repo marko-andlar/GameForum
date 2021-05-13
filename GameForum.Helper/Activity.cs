dc50725d239189ac6ce58608a062e2878c669116
Marko <marko.andlar@gmai.com>
2021-05-13 09:35:21 +0200

    Added Helper Library

---
+++ GameForum.Helper/Activity.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace GameForum.Helper
+{
+    public static class Activity
+    {
+        public static string FromDates(DateTime dateCreated, DateTime? dateEdited)
+        {
+            return dateEdited is null ? DateToActivity(dateCreated) : DateToActivity((DateTime)dateEdited);
+        }
+        public static string DateToActivity(DateTime date)
+        {
+            var elapsedTime = DateTime.Now - date;
+            if (elapsedTime.TotalDays >= 365)
+            {
+                return (int)elapsedTime.TotalDays / 365 + "y";
+            }
+            else if (elapsedTime.TotalDays >= 30)
+            {
+                return (int)elapsedTime.TotalDays / 30 + "m";
+            }
+            else if (elapsedTime.TotalDays >= 1)
+            {
+                return (int)elapsedTime.TotalDays + "d";
+            }
+            else if (elapsedTime.TotalMinutes >= 1)
+            {
+                return (int)elapsedTime.TotalMinutes + "m";
+            }
+            else
+            {
+                return (int)elapsedTime.TotalSeconds + "s";
+            }
+        }
+    }
+}
